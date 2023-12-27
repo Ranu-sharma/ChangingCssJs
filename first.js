779d18e724dc3883e75ffe940ffab2ffeca4b762
Ranu-sharma <152103726+Ranu-sharma@users.noreply.github.com>
2023-12-27 23:03:38 +0100

    Add files via upload

---
+++ first.js
@@ -0,0 +1,26 @@
+
+const student = {
+    fullName: "Rahul Kumar",
+    age: 20,
+   cgpa: 8.2,
+    ispass: true,
+};
+console.log(student);
+
+
+const product = {
+    title : "Ball pen",
+    rating : 4,
+    offer : 5,
+    price : 270,
+} 
+console.log(product);
+
+const profile = {
+    userName : "Ranu Sharma",
+    followers : 500,
+    post : 50,
+    isFollow : false,
+
+}
+console.log (type of profile["followers"]);m
